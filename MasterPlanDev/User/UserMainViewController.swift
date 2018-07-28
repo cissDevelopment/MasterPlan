4bd3cc627473b7782061c168b7c81d6e625bf0cd
Joshua Shou <joshua2019175@concordiashanghai.org>
2018-07-28 13:53:45 +0800

    user

--- MasterPlanDev/User/UserMainViewController.swift
+++ MasterPlanDev/User/UserMainViewController.swift
@@ -1,17 +1,10 @@
 //
-
 //  UserMainViewController.swift
-
 //  MasterPlanDev
-
 //
-
 //  Created by Yoochan Shin on 2018/7/24.
-
 //  Copyright © 2018 YoochanShin. All rights reserved.
-
 //
-
 
 import UIKit
 
@@ -103,164 +96,15 @@ class UserMainViewController: UIViewController, UIImagePickerControllerDelegate,
     
     
     
-    
-    
-    let firstname : UILabel = {
-        
-        let label = UILabel()
-        
-        label.text = "Joshua"
-        
-        label.adjustsFontSizeToFitWidth = true
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        label.font = UIFont.init(name: "Avenir-Heavy", size : 50)
-        
-        label.textColor = .white
-        
-        label.adjustsFontSizeToFitWidth = true
-        
-        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        
-        label.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        
-        
-        
-        
-        
-        return label
-        
-    }()
-    
-    
-    
-    let view1 : UIView = {
-        
-        let view = UIView()
-        
-        
-        
-        return view
-        
-        
-        
-    }()
-    
-    
-    
-    let lastname : UILabel = {
-        
-        let label = UILabel()
-        
-        label.text = "Shou"
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        label.textColor  = .white
-        
-        label.font = UIFont.init(name: "Avenir-Heavy", size:50)
-        
-        label.sizeToFit()
-        
-        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        
-        label.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        
-        
-        
-        
-        
-        return label
-        
-    }()
-    
-    
-    
-    let profilepic: UIImageView  = {
-        
-        let image = UIImageView()
-        
-        image.backgroundColor = mainBlue
-        
-        image.layer.borderWidth = 0
-        
-        image.layer.masksToBounds = false
-        
-        image.layer.borderColor = UIColor.black.cgColor
-        
-        image.layer.cornerRadius = 75
-        
-        image.clipsToBounds = true
-        
-        image.translatesAutoresizingMaskIntoConstraints = false
-        
-        image.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        
-        image.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        
-        return image
-        
-    }()
-    
-    
-    
-    let changepic: UIButton = {
-        
-        let image1 = UIButton()
-        
-        image1.setImage(#imageLiteral(resourceName: "Ryan Gosling"), for: .normal)
-        
-        image1.layer.borderWidth = 0
-        
-        image1.layer.masksToBounds = false
-        
-        image1.layer.borderColor = UIColor.black.cgColor
-        
-        image1.layer.cornerRadius = 60
-        
-        image1.clipsToBounds = true
-        
-        image1.translatesAutoresizingMaskIntoConstraints = false
-        
-        image1.widthAnchor.constraint(equalToConstant: 120).isActive = true
-        
-        image1.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        
-        // image1.addTarget(self, action: #selector(changepicAction(sender:)), for: .touchUpInside)
-        
-        
-        
-        
-        
-        return image1
-        
-    }()
-    
-    
-    
-    
-    
-    
-    
     let userSegmentedControl: UISegmentedControl = {
-        
         let userItems = ["Stats", "Current", "Settings"]
-        
         let segmentedControl = UISegmentedControl(items: userItems)
         
-        
-        
         segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
-        
-        
         
         return segmentedControl
         
     }()
-
-    
-    
     
     
     
@@ -270,21 +114,18 @@ class UserMainViewController: UIViewController, UIImagePickerControllerDelegate,
     
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-        
         
         view.addSubview(userSegmentedControl)
         view.addSubview(view1)
         view.addSubview(view2)
         view.addSubview(view3)
         
-
+        
         userSegmentedControl.addUnderlineForSelectedSegment()
         
         view1.translatesAutoresizingMaskIntoConstraints = false
-
+        
         view1.topAnchor.constraint(equalTo: userSegmentedControl.bottomAnchor).isActive = true
         view1.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -49).isActive = true
         view1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -306,19 +147,19 @@ class UserMainViewController: UIViewController, UIImagePickerControllerDelegate,
         
         lastname.leftAnchor.constraint(equalTo: changepic.rightAnchor, constant:30).isActive = true
         lastname.topAnchor.constraint(equalTo: firstname.topAnchor, constant:50).isActive = true
-
-}
+        
+    }
     
     
     
     
     
     
-@objc func selectionDidChange(_ sender: UISegmentedControl) {
-    updateView()
-    userSegmentedControl.changeUnderlinePosition()
-}
-
+    @objc func selectionDidChange(_ sender: UISegmentedControl) {
+        updateView()
+        userSegmentedControl.changeUnderlinePosition()
+    }
+    
     
     private func setupLayoutview1(){
         userSegmentedControl.changeUnderlinePosition()
@@ -355,16 +196,16 @@ class UserMainViewController: UIViewController, UIImagePickerControllerDelegate,
         
     }
     
-
+    
     @objc func changepicAction (sender: UIButton!) {
         print ("something!")
         imagepicker.delegate = self
         imagepicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
         
         self.present(imagepicker, animated:true, completion:nil)
-
-    
-    
+        
+        
+        
     }
     
     func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String:Any]){
@@ -403,10 +244,9 @@ class UserMainViewController: UIViewController, UIImagePickerControllerDelegate,
             print("view 3!")
             setupLayoutView3()
         }
+    }
+    
+    
+    
+    
 }
-
-
-
-
-}
-
