4af61d8a13b6cc14fb7ba5a13a726efa702e1b21
Yoochan Shin <yoochanpshin@gmail.com>
2018-07-28 13:11:26 +0800

    Bar and Settings

--- MasterPlanDev/User/UserSettingsViewController.swift
+++ MasterPlanDev/User/UserSettingsViewController.swift
@@ -9,11 +9,77 @@
 import UIKit
 
 class UserSettingsViewController: UIViewController {
+    
+    let profile : UIButton = {
+        let button = UIButton()
+        
+        button.layer.borderColor = mainBlue.cgColor
+        button.layer.borderWidth = 10
+        button.layer.cornerRadius = 75
+        button.setImage(#imageLiteral(resourceName: "Ryan Gosling"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    let firstName : UILabel = {
+        let label = UILabel()
+        
+        label.text = "Joshua"
+        
+        label.textColor = .white
+        label.font = UIFont(name: "Avenir-Heavy", size: 50)
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let lastName : UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .white
+        label.font = UIFont(name: "Avenir-Heavy", size: 50)
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let scrollView : UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.backgroundColor = lightBlue
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = lightBlue
+        
+        view.addSubview(scrollView)
+        
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 2000)
+        
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        scrollView.addSubview(profile)
+        
+        profile.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 15).isActive = true
+        profile.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 15).isActive = true
+        profile.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        profile.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        
+        
     }
 
 }
