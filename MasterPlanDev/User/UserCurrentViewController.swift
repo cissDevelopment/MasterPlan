cfb2bcdd17c2ea6a53c27f0f232f29c47c19e30e
doublerch <doublerch@gmail.com>
2018-07-30 12:30:28 +0800

    Created More Menu

--- MasterPlanDev/User/UserCurrentViewController.swift
+++ MasterPlanDev/User/UserCurrentViewController.swift
@@ -1,24 +1,14 @@
 //
-
 //  UserCurrentViewController.swift
-
 //  MasterPlanDev
-
 //
-
 //  Created by Yoochan Shin on 2018/7/25.
-
 //  Copyright © 2018 YoochanShin. All rights reserved.
-
 //
-
 
 import UIKit
 
 class UserCurrentViewController: UITableViewController {
-
-class UserCurrentViewController: UITableViewController {
-    
     
     override func viewDidLoad() {
         
@@ -43,22 +33,4 @@ class UserCurrentViewController: UITableViewController {
         return tableView.dequeueReusableCell(withIdentifier: "service", for: indexPath)
     }
     
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 5
-        
-    }
-    
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        return tableView.dequeueReusableCell(withIdentifier: "service", for: indexPath)
-        
-    }
-    
-    
 }
-
