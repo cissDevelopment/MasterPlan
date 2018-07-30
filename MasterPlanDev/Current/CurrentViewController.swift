bf0fe887ed9dce701af65e00a9744c08cae3d058
Joshua Shou <joshua2019175@concordiashanghai.org>
2018-07-30 11:55:12 +0800

    CHANGES

---
+++ MasterPlanDev/Current/CurrentViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class CurrentViewController: UITableViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        
+        
+        tableView.register(serviceCell.self, forCellReuseIdentifier: "service")
+        tableView.rowHeight = 200
+        
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.separatorColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.separatorInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.backgroundColor = lightBlue
+    
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "service", for: indexPath)
+    }
+    
+}
