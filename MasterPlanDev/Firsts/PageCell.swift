abaf400659161db4fe76f105479c2cbb249bd7ef
Joshua Shou <joshua2019175@concordiashanghai.org>
2018-08-13 14:16:35 +0800

    a

--- MasterPlanDev/Firsts/PageCell.swift
+++ MasterPlanDev/Firsts/PageCell.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class PageCell: UICollectionViewCell {
     
+    let introductionLabel : UILabel = {
+        
+        let label = UILabel()
+        label.text = "Introduction"
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.init(name: "Avenir-Heavy", size : 50)
+        label.textColor = .white
+        label.adjustsFontSizeToFitWidth = true
+        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        return label
+    }()
+
+    let descriptionTextView : UITextView = {
+        let textView = UITextView()
+        textView.textColor = .black
+        textView.text = "filler"
+        textView.font = UIFont(name: "Avenir-Medium", size: 15)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .white
+        textView.isEditable = false
+        return textView
+    }()
+    
     let userImageView : UIImageView = {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "User"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
+
         return imageView
         
     }()
@@ -23,18 +49,18 @@ class PageCell: UICollectionViewCell {
         let button = UIButton()
         
         button.setAttributedTitle(NSAttributedString(string: "Back", attributes: [.font : UIFont(name: "Avenir-Black", size: 20)!]), for: .normal)
-        button.titleLabel?.textColor = .white
+        button.titleLabel?.textColor = .black
         
         button.translatesAutoresizingMaskIntoConstraints = false
        // button.addTarget(self, action: #selector(seguetoBarViewController), for: .touchUpInside)
-        
+        button.backgroundColor = lightBlue
         return button
         
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .purple
+        backgroundColor = secondaryBlue
         setupLayout()
 
         
@@ -43,24 +69,46 @@ class PageCell: UICollectionViewCell {
     
     private func setupLayout()
     {
-        let topImageContainerView = UIView()
-        addSubview(topImageContainerView)
+        
+        let introview = UIView()
         addSubview(backButton)
-        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(introductionLabel)
+        addSubview(descriptionTextView)
+        addSubview(userImageView)
+        addSubview(introview)
+        introview.translatesAutoresizingMaskIntoConstraints = false
         
-        topImageContainerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        topImageContainerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        topImageContainerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        introview.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        introview.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        introview.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        introview.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+
+
+
+        introductionLabel.topAnchor.constraint(equalTo: introview.topAnchor, constant:20).isActive = true
+        introductionLabel.bottomAnchor.constraint(equalTo: introview.topAnchor, constant:95).isActive = true
+        introductionLabel.centerXAnchor.constraint(equalTo: introview.centerXAnchor).isActive = true
+        
+
+        userImageView.topAnchor.constraint(equalTo: introductionLabel.bottomAnchor, constant: 10).isActive = true
+        userImageView.leftAnchor.constraint(equalTo: introview.leftAnchor, constant: 100).isActive = true
+        userImageView.rightAnchor.constraint(equalTo: introview.rightAnchor, constant: -100).isActive = true
+        userImageView.bottomAnchor.constraint(equalTo: userImageView.topAnchor, constant: 354).isActive = true
         
         
-        topImageContainerView.addSubview(userImageView)
-        userImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
-        userImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
-        userImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 50).isActive = true
         
-        topImageContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
-        backButton.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant:50).isActive = true
-        
+        descriptionTextView.leftAnchor.constraint(equalTo: introview.leftAnchor, constant: 50).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: introview.rightAnchor, constant:-50).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: introview.bottomAnchor)
+
+//
+//
+        backButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant:20).isActive = true
+        backButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        backButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 50).isActive = true
+        backButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -50).isActive = true
+
         
     }
     
--- MasterPlanDev/Firsts/SwipingController.swift
+++ MasterPlanDev/Firsts/SwipingController.swift
@@ -4,7 +4,13 @@ import UIKit
 
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    let imageNames = ["User", "Request", "Ryan Gosling", "More"]
+    let imageNames = ["MasterPlan", "Home Page", "User Page", "Current Page", "Request Page"]
+    
+    let strings = ["Welcome to MasterPlan! This is the introduction to how to use the App! Each of the 4 tabs on the bottom refers to a different page",
+                   "The first tab is the Service Bar page. Here you can see all the service requests, and sign up for whichever slot you want.",
+                   "The second tab is the Request page. Here you can request your service, be sure to fill out all the necessary information.",
+                   "The third tab is the current page, here you can see all the services you signed up for, organized for you in rows.",
+                   "Finally, the fourth tab is the user page, here you can edit your profile pic, change your notification settings, and see your accumulated service hours."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +21,8 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         
         collectionView?.isPagingEnabled = true
+        
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -35,7 +43,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         let imageName = imageNames[indexPath.item]
         cell.userImageView.image = UIImage(named: imageName)
         cell.backButton.addTarget(self, action: #selector(seguetoBarViewController), for: .touchUpInside)
-        
+        cell.descriptionTextView.text = strings[indexPath.item]
         
         return cell
     }
